449d1e164ebbb833cb866167a7edcf065041f128
Mudith-Jayasanka <mudith.jayasanka@gmail.com>
2021-05-04 16:01:46 +0700

    firebase config added to second env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyByK1ypAhpWrpfQ1qUDLx80Aqm9N3vRcfs",
+    authDomain: "angular-playground-f633c.firebaseapp.com",
+    projectId: "angular-playground-f633c",
+    storageBucket: "angular-playground-f633c.appspot.com",
+    messagingSenderId: "759234116334",
+    appId: "1:759234116334:web:189d2f6c3f907942ddd1ff"
+  }
 };
